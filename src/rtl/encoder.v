fe8bd8a1e384167edbae605fec2ba0251acfe9ab
Youssif991 <youssif.sayed05@eng-st.cu.edu.eg>
2026-08-24 22:13:03 +0300

    feat: add encoder, decoder, convolver, multiplier RTL and testbenches

---
+++ src/rtl/encoder.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Youssef
+//
+// Create Date: MM/DD/YYYY
+// Design Name: Design Name
+// Module Name: module_name
+// Tool Versions: Vivado 2025.2
+// Description: Description
+//
+// Dependencies: module (path)
+//
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+module encoder(
+    input wire [7:0] data_i,
+    output wire [2:0] seq1_o,
+    output wire [2:0] seq2_o,
+    output wire [2:0] seq3_o,
+    output wire [2:0] seq4_o
+);
+endmodule
---
+++ src/rtl/multiplier.v
@@ -0,0 +1,22 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Youssef
+//
+// Create Date: MM/DD/YYYY
+// Design Name: Design Name
+// Module Name: module_name
+// Tool Versions: Vivado 2025.2
+// Description: Description
+//
+// Dependencies: module (path)
+//
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+module multiplier(
+    
+);
+endmodule
